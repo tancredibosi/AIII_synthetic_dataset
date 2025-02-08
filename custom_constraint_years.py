d24759a450fb014fd177088146827a17a3af8b4d
TancrediBosi <tancredi.bosi@gmail.com>
2025-02-08 17:39:16 +0100

    custom constraints update

--- custom_constraint_years.py
+++ custom_constraint_years.py
@@ -4,7 +4,9 @@ from sdv.constraints import create_custom_constraint_class
 
 def is_valid(column_names, data):
     """
-    Check if Year of insertion <= Year of Recruitment when Candidate State is 'Hired'
+    Check two conditions:
+    1. If Candidate State is 'Hired': Year of insertion <= Year of Recruitment
+    2. If Candidate State is not 'Hired': Year of Recruitment must be NaN
     
     Args:
         column_names: List containing ['Candidate State', 'Year of insertion', 'Year of Recruitment']
@@ -13,14 +15,18 @@ def is_valid(column_names, data):
     Returns:
         pd.Series: Boolean series with True for valid rows
     """
-    # Create a mask for rows where Candidate State is 'Hired'
+    # Create masks
     hired_mask = data[column_names[0]] == 'Hired'
+    not_hired_mask = ~hired_mask
     
     # Initialize result series with True
     validity = pd.Series(True, index=data.index)
     
-    # For hired candidates, check if Year of insertion <= Year of Recruitment
+    # Condition 1: For hired candidates, check if Year of insertion <= Year of Recruitment
     validity[hired_mask] = data[column_names[1]][hired_mask] <= data[column_names[2]][hired_mask]
+    
+    # Condition 2: For non-hired candidates, check if Year of Recruitment is NaN
+    validity[not_hired_mask] = data[column_names[2]][not_hired_mask].isna()
     
     return validity
 
