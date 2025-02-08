3f5ce715b2c69be482987a2c2a33545309852606
TancrediBosi <tancredi.bosi@gmail.com>
2025-02-08 17:30:20 +0100

    custom constraint

--- custom_constraint_years.py
+++ custom_constraint_years.py
@@ -3,15 +3,28 @@ from sdv.constraints import create_custom_constraint_class
 
 
 def is_valid(column_names, data):
+    """
+    Check if Year of insertion <= Year of Recruitment when Candidate State is 'Hired'
+    
+    Args:
+        column_names: List containing ['Candidate State', 'Year of insertion', 'Year of Recruitment']
+        data: DataFrame containing the columns
+        
+    Returns:
+        pd.Series: Boolean series with True for valid rows
+    """
+    # Create a mask for rows where Candidate State is 'Hired'
+    hired_mask = data[column_names[0]] == 'Hired'
+    
+    # Initialize result series with True
+    validity = pd.Series(True, index=data.index)
+    
+    # For hired candidates, check if Year of insertion <= Year of Recruitment
+    validity[hired_mask] = data[column_names[1]][hired_mask] <= data[column_names[2]][hired_mask]
+    
+    return validity
 
-    if data[column_names[0]] == 'Hired':
-        validity = data[column_names[1]] <= data[column_names[2]]
-    else:
-        validity = data[column_names[2]].isna()
-
-    return pd.Series(validity)
-
-MyCustomConstraintClass = create_custom_constraint_class(
+CustomYearsHired = create_custom_constraint_class(
     is_valid_fn=is_valid,
     # transform_fn=transform, # optional
     # reverse_transform_fn=reverse_transform # optional
